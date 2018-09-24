d8ba7ad8caec52ed659494ed2b8a323510fab5dc
Workbook <workbook@workbooks-macbook-pro.local>
2018-09-24 13:52:40 -0400

    new files

--- cis357hw3/ViewController.swift
+++ cis357hw3/ViewController.swift
@@ -9,27 +9,62 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+//protocol lengthSelectionViewController{
+//    func indicateSelection(vice: String)
+//
+//}
+
+class ViewController: UIViewController, UITextFieldDelegate{
+    
 
     
-    @IBOutlet weak var yardsField: UITextField!
+    @IBOutlet weak var yardsField: DecimalMinusTextField!
+    //UITextField!
     
     
-    @IBOutlet weak var metersField: UITextField!
+    @IBOutlet weak var metersField: DecimalMinusTextField!
+    
+   // var pickerdata : [String] = [String]()
+ 
+    
+
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+
         // Do any additional setup after loading the view, typically from a nib.
         
         // dismiss keyboard when tapping outside oftext fields
         let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
         self.view.addGestureRecognizer(detectTouch)
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        if let d = self.delegate {
+//            d.indicateSelection(vice: selection)
+//        }
+//    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let dest = segue.destination as? ViewController{
+//
+//            dest.delegate = self
+//        }
+//    }
+//    func indicateSelection(vice: String) {
+//        self.fromUnits.text = vice
+//    }
     
     @objc func dismissKeyboard() {
         self.view.endEditing(true)
@@ -37,36 +72,35 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         var yardsOk = false
-        var metersOk = false
-        if let yards = self.yardsField.text, let meters = self.metersField.text  {
-            if yards != "" && meters != "" {
+
+        if let yards = self.yardsField.text {
+            if yards != "" {
                 yardsOk = true
-                metersOk = true
+              
             }
         }
-        if !yardsOk && !metersOk {
-            print("Please enter length in either yards or meters!")
+        if !yardsOk {
+            print("Please enter a length!")
         }
+        
+        
+        
+        
     }
     
     
-    
-    
-    @IBAction func signupButtonPressed(_ sender: UIButton) {
-        /*
-        var emailOk = false
-        if let email = self.emailField.text {
-            let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-            
-            let emailPredicate = NSPredicate(format:"SELF MATCHES %@", regex)
-            emailOk = emailPredicate.evaluate(with: email)
-        }
-        if !emailOk {
-            print("Invalid email address")
- 
-        }
- */
-    }
-    
+
 }
+
+
+
+
+
+
+
+
+
+    
+    
+
 
